449fc5d173b2e8db69a2edf728801f13cacfbdec
= <pmuriuki98@gmail.com>
2018-10-14 03:10:10 +0300

    finding best f1 score for optimum epsilon

--- mlex8/ex8/selectThreshold.m
+++ mlex8/ex8/selectThreshold.m
@@ -23,15 +23,17 @@ for epsilon = min(pval):stepsize:max(pval)
     % Note: You can use predictions = (pval < epsilon) to get a binary vector
     %       of 0's and 1's of the outlier predictions
 
+temp = pval < epsilon;
 
-
-
-
-
-
-
-
-
+fp = sum((temp == 1) & (yval == 0));
+fn = sum((temp == 0) & (yval == 1));
+tn = sum((temp == 0) & (yval == 0));
+tp = sum((temp == 1) & (yval == 1));
+    
+  prec = tp / (tp + fp);
+  rec = tp / (tp + fn);
+  
+  F1 = 2*prec*rec/(prec + rec);
 
 
 
